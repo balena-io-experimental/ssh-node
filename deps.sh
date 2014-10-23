aeee56778fa99484e690f9e9f8099754b7082a51
Lorenzo Stoakes <lstoakes@gmail.com>
2014-10-23 11:56:39 +0100

    Refer to cowsay installed path.

--- deps.sh
+++ deps.sh
@@ -4,4 +4,4 @@ set -o errexit
 
 apt-get -q update
 apt-get install -y cowsay
-cowsay "Install dependencies using a script like this!"
+/usr/games/cowsay "Install dependencies using a script like this!"
