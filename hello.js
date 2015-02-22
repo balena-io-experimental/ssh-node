aa5a10d3752e5ac7e83cf20dae451e69d9b53af2
Craig Mulligan <craig@firingsuad.co.za>
2015-02-22 15:48:17 +0000

    without server

--- hello.js
+++ hello.js
@@ -1,10 +1,10 @@
 var http = require('http');
 
 console.log('hi')
-http.createServer(function (request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin' : '*'
-    });
-    response.end('Hello World\n');
-}).listen(8080);
+// http.createServer(function (request, response) {
+//     response.writeHead(200, {
+//         'Content-Type': 'text/plain',
+//         'Access-Control-Allow-Origin' : '*'
+//     });
+//     response.end('Hello World\n');
+// }).listen(8080);
